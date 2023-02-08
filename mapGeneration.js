28c6459a7b27c1d659eddc0780f37b66e541fd6f
Gustavo Villar <105249607+gustavo-villar-dev@users.noreply.github.com>
2023-02-08 20:11:56 -0300

    huge update: made it on raw javascript no need for p5, deleted game it will now be just a perlin noise generator tool

--- mapGeneration.js
+++ mapGeneration.js
@@ -1,65 +1,118 @@
-/* 
-    this script makes a height map using perlin noise
-
-    TODO:
-    define that for each height we have certain biome or color
-*/
-
-import p5 from "p5";
-
 // --------------------------------------
 //              GENERATE
 //              TERRAIN
 // --------------------------------------
 
-// set the number of columns and rows
-const columns = 100;
-const rows = 100;
+function generateNoise(xCoord, yCoord) {
+  let totalNoise = 0.2;
+  let persistence = 0.6;
+  let octaveCount = 10;
+  for (let currentOctave = 0; currentOctave < octaveCount; currentOctave++) {
+    let frequency = 2**currentOctave;
+    let amplitude = persistence**currentOctave;
+    totalNoise += generatePerlinNoise(xCoord * frequency, yCoord * frequency) * amplitude;
+  }
+  return totalNoise;
+}
 
-// create the heights array
-let heights;
+// The Perlin noise algorithm
+function generatePerlinNoise(xCoord, yCoord) {
+  let xIntCoord = Math.floor(xCoord);
+  let yIntCoord = Math.floor(yCoord);
+  let xFracCoord = xCoord - xIntCoord;
+  let yFracCoord = yCoord - yIntCoord;
+  let xInterp = fade(xFracCoord);
+  let yInterp = fade(yFracCoord);
+  let topLeftNoise = gradient(hashCoordinates(xIntCoord, yIntCoord), xFracCoord, yFracCoord);
+  let topRightNoise = gradient(hashCoordinates(xIntCoord + 1, yIntCoord), xFracCoord - 1, yFracCoord);
+  let bottomLeftNoise = gradient(hashCoordinates(xIntCoord, yIntCoord + 1), xFracCoord, yFracCoord - 1);
+  let bottomRightNoise = gradient(hashCoordinates(xIntCoord + 1, yIntCoord + 1), xFracCoord - 1, yFracCoord - 1);
+  let xInterpTop = lerp(topLeftNoise, topRightNoise, xInterp);
+  let xInterpBottom = lerp(bottomLeftNoise, bottomRightNoise, xInterp);
+  return lerp(xInterpTop, xInterpBottom, yInterp);
+}
 
-// generate the heights for each cell
-const generateHeights = () => {
-    heights = [];
+// Interpolation function
+function lerp(startValue, endValue, interpAmount) {
+  return (1 - interpAmount) * startValue + interpAmount * endValue;
+}
 
-    for (let x = 0; x < columns; x++) {
-        heights[x] = [];
+// Smoothstep function
+function fade(interpAmount) {
+  return interpAmount * interpAmount * interpAmount * (interpAmount * (interpAmount * 6 - 15) + 10);
+}
 
-        for (let y = 0; y < rows; y++) {
-            heights[x][y] = p5.noise(x * 0.1, y * 0.1);
-        }
-    }
-};
+let hashX = 12.9898;
+let hashY = 78.233;
+let hashZ = 43758.5453;
+
+// Hash function to determine gradient
+function hashCoordinates(xIntCoord, yIntCoord) {
+  let hash = Math.sin(xIntCoord * hashX + yIntCoord * hashY) * hashZ;
+  return hash - Math.floor(hash);
+}
+
+// Gradient function to determine the intensity of the noise
+function gradient(hash, xFracCoord, yFracCoord) {
+  switch (Math.floor(hash * 8) % 8) {
+    case 0:
+      return xFracCoord + yFracCoord;
+    case 1:
+      return -xFracCoord + yFracCoord;
+    case 2:
+      return xFracCoord - yFracCoord;
+    case 3:
+      return -xFracCoord - yFracCoord;
+    case 4:
+      return xFracCoord + yFracCoord;
+    case 5:
+      return -xFracCoord + yFracCoord;
+    case 6:
+      return xFracCoord - yFracCoord;
+    case 7:
+      return -xFracCoord - yFracCoord;
+    default:
+      return 0;
+  }
+}
+
 
 // --------------------------------------
 //              DISPLAY
 //              TERRAIN
 // --------------------------------------
 
-// set the size of each cell
-const cellSize = 10;
 
-// create a new p5.js sketch
-const sketch = (p) => {
+// Set the dimensions of the map
+const mapWidth = 1080;
+const mapHeight = 1080;
 
-  // initialize the sketch
-  p.setup = () => {
-    p.createCanvas(columns * cellSize, rows * cellSize);
-    generateHeights();
-  };
+function generateNewMap(){
 
-  // draw the terrain
-  p.draw = () => {
-    for (let x = 0; x < columns; x++) {
-        for (let y = 0; y < rows; y++) {
-            let h = heights[x][y];
-            p.fill(h * 255);
-            p.rect(x * cellSize, y * cellSize, cellSize, cellSize);
-        }
+  // Create a 2D array to store the noise values
+  let noiseValues = [];
+  for (let x = 0; x < mapWidth; x++) {
+    noiseValues[x] = [];
+    for (let y = 0; y < mapHeight; y++) {
+      noiseValues[x][y] = generateNoise(x / mapWidth, y / mapHeight);
     }
-  };
-};
+  }
 
-// create a new p5.js instance
-new p5(sketch);
+  // Create a canvas to display the map
+  let canvas = document.getElementById("gameCanvas");
+  canvas.width = mapWidth;
+  canvas.height = mapHeight;
+  document.body.appendChild(canvas);
+  let context = canvas.getContext("2d");
+
+  // Fill the canvas with the noise values
+  for (let x = 0; x < mapWidth; x++) {
+    for (let y = 0; y < mapHeight; y++) {
+      let noiseValue = noiseValues[x][y];
+      context.fillStyle = `rgb(${noiseValue * 255}, ${noiseValue * 255}, ${noiseValue * 255})`;
+      context.fillRect(x, y, 1, 1);
+    }
+  }
+}
+
+generateNewMap();
